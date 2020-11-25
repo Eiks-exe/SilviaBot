9c2184b56dba9ea15cae690b784f35bff5ab01ee
NWZX <nwzx@users.noreply.github.com>
2020-11-24 20:51:57 -0400

    🌈Darling in the franxx

--- src/SiviaCmd/Player.ts
+++ src/SiviaCmd/Player.ts
@@ -1,4 +1,4 @@
-import ytS from 'yt-search';
+import ytS, { VideoSearchResult } from 'yt-search';
 import youtubeStream from 'ytdl-core';
 import Discord, { Message } from 'discord.js';
 import { Queue } from '../Tool/Queue';
@@ -10,31 +10,26 @@ export default class player {
         console.log(this.queue);
     }
 
-    async play(SearchMsg: string, message: Message, url?: string): Promise<void> {
+    async play(SearchMsg: string, message: Message, nextPlay?: string): Promise<void> {
         try {
-            const firstResult = { url: '' };
-            if (url) {
-                firstResult.url = url;
+            let itemUrl = '';
+            if (nextPlay) {
+                const { url, title, timestamp, image } = await ytS({
+                    videoId: nextPlay.replace('https://youtube.com/watch?v=', ''),
+                });
+                itemUrl = url;
+                this.playMessage('playing', title, timestamp, url, image, message);
             } else {
-                const r = await ytS(SearchMsg);
-                const videos = r.videos;
-                firstResult.url = videos[0].url;
+                const { url, title, timestamp, image } = await (await ytS(SearchMsg)).videos[0];
+                itemUrl = url;
+
+                this.playMessage('add playlist', title, timestamp, url, image, message);
             }
+
             if (message.member?.voice.channel) {
                 const connection = await message.member.voice.channel.join();
-
-                const embed = new Discord.MessageEmbed();
-                embed.setColor('#00cc00');
-                embed.title = 'playing';
-                embed.description = firstResult.title;
-                embed.addField('duration', firstResult.timestamp);
-                embed.addField('link', firstResult.url);
-                embed.setThumbnail(firstResult.image);
-
-                if (!url) message.delete();
-                message.channel.send(embed);
-                if (firstResult.url != this.queue.peek()) this.queue.enqueue(firstResult.url);
-                if (firstResult.url === this.queue.peek()) {
+                if (itemUrl != this.queue.peek()) this.queue.enqueue(itemUrl);
+                if (itemUrl === this.queue.peek()) {
                     console.log('startplaying ' + this.queue.peek());
                     const stream = youtubeStream(this.queue.peek() || '');
                     const action = async () => {
@@ -58,5 +53,24 @@ export default class player {
         } catch (error) {
             console.error(error);
         }
+    }
+
+    private playMessage(
+        status: 'playing' | 'add playlist',
+        title: string,
+        timestamp: string,
+        url: string,
+        image: string,
+        message: Discord.Message,
+    ) {
+        const embed = new Discord.MessageEmbed();
+        embed.setColor('#00cc00');
+        embed.title = status;
+        embed.description = title;
+        embed.addField('duration', timestamp);
+        embed.addField('link', url);
+        embed.setThumbnail(image);
+        message.channel.send(embed);
+        if (status == 'add playlist') message.delete();
     }
 }
