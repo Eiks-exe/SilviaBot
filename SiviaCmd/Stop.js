81917a5e964246175845452da69ff1f4946e54c0
Eiks-exe <66579704+Eiks-exe@users.noreply.github.com>
2020-12-20 00:26:11 -0400

    Stop

--- SiviaCmd/Stop.js
+++ SiviaCmd/Stop.js
@@ -1,6 +1,6 @@
 module.exports = class stop {
     static match(message) {
-        return message.content.startsWith(`${process.env.PREFIX} stop `)
+        return message.content.startsWith(`${process.env.PREFIX} stop`)
     }
     static action(message ,lect) {
         lect.stop(message)
