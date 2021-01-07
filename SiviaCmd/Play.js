918a85f21d892f443e8dba644810006b0dd00dd2
Eiks-exe <66579704+Eiks-exe@users.noreply.github.com>
2021-01-07 00:11:23 -0400

    Update Play.js

--- SiviaCmd/Play.js
+++ SiviaCmd/Play.js
@@ -10,8 +10,15 @@ module.exports = class temp {
     }
 
     static action(message, lect) {
-        let args = message.content.split(`${process.env.PREFIX} play `) // crée un array avec le contenu du msg séparé par un ' ' 
-        lect.play(args[1], message)
+        if(message.content.startsWith(`${process.env.PREFIX} play `)){
+            let args = message.content.split(`${process.env.PREFIX} play `)
+            lect.play(args[1], message)   
+        } else if(message.content.startsWith(`${process.env.MOBPREFIX}play `)){
+            let args = message.content.split(`${process.env.MOBPREFIX}play `)
+            lect.play(args[1], message)
+        }
+         // crée un array avec le contenu du msg séparé par un ' ' 
+        
         // reconversion du tableau en chaine de caractères
         //console.log(StrArgs)
 
