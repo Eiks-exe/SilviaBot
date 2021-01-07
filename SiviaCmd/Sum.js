e5293c3272b0c4351d14512d2c69c3c60ff61d58
Eiks-exe <66579704+Eiks-exe@users.noreply.github.com>
2021-01-06 23:10:10 -0400

    added gif for summoning

--- SiviaCmd/Sum.js
+++ SiviaCmd/Sum.js
@@ -1,15 +1,20 @@
 const Discord = require('discord.js');
+const giphyApi = require('giphy-api');
+const gif = require('giphy-api')();
 
 const client = new Discord.Client();
-var gif = require('giphy-api')();
 module.exports = class Sum {
     static match(message) {
         return message.content.startsWith('ninpo ')
     }
 
     static action(message) {
+        gif.id('Mscw2tH9hcAne', function (err, res){
+            
+        });
+        
         let dm = message.mentions.users.first();
-
+        
         let a = message.content.split(' ')
         let summoned = a[a.length - 1]
         let embed = new Discord.MessageEmbed()
@@ -17,13 +22,14 @@ module.exports = class Sum {
             .setTitle('Hellow')
             .setDescription('**you have been summoned by:  **' + Object.values(message.author)[4] + ' in: ' + message.channel.name)
             .setThumbnail('https://cdn.discordapp.com/avatars/598581926898696203/61e90b2c4f5c6cfef254c053e873a5cc.webp')
+            .setImage('https://giphy.com/gifs/naruto-uzumaki-shippuuden-Mscw2tH9hcAne')
         //console.log(member)
         console.log(message.author)
         dm.send(embed)
             .then(done => {
                 message.delete()
 
-                message.channel.send('ninpo: invocation ' + summoned)
+                message.channel.send('ninpo: invocation ' + summoned )
 
             })
             .catch(console.log)
