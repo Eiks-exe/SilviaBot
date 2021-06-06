74cf4d0fe7bc3b0904aeb11ecf6a7feef1c98d69
Eiks-exe <eiks_art@outlook.com>
2021-06-05 23:08:51 -0400

    leave-fix

--- src/SiviaCmd/Leave.ts
+++ src/SiviaCmd/Leave.ts
@@ -6,8 +6,8 @@ export default class Leave {
     }
 
     static action(message: Message): void {
-        if (message.member && message.member.voice.channel) {
-            if (message.guild && message.member.voice.connection) {
+        if (message.member?.voice.channel) {
+            if (message.guild?.voice?.connection) {
                 message.member.voice.channel.leave();
                 message.channel.send('leaved');
                 message.delete();
